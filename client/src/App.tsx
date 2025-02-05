3dd3db23df78fecfa0e8e969c6be6f5a4694180c
Tanner Chamberlain <tannerchamberlain8@gmail.com>
2025-02-04 19:38:44 -0700

    landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import SuperButton from "./components/ui/button";
+import "./App.css";
+// import { Input } from "@/components/ui/input";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    
+    <h1 className="loginHeader">UNRIVALED</h1>
+      {/* Navbar */}
+      <nav className="nav">
+        <div className="">
+          <input type="text" placeholder="Username" className="w-32"></input>
+          <input type="password" placeholder="Password" className="w-32" ></input>
+        </div>
+      </nav>
 
-export default App
+      {/* Main Content */}
+      <main className="">
+        <div className="">
+          <SuperButton/><SuperButton/><SuperButton/>
+          {}
+        </div>
+      </main>
+
+      {/* Footer */}
+      <footer className="">
+        <p className="">All rights reserved.</p>
+      </footer>
+    </>
+  );
+};
+
+export default App;
